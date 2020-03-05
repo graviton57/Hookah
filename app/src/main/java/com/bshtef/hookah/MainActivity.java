a58c7ed859224272cfa21db0eb484e38264e4065
shtefunik <shtefunik@mdcm.co>
2020-03-05 18:00:33 +0200

    add base screen with menu

--- app/src/main/java/com/bshtef/hookah/MainActivity.java
+++ app/src/main/java/com/bshtef/hookah/MainActivity.java
@@ -1,15 +1,113 @@
 package com.bshtef.hookah;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle drawerToggle;
+
+    //region ******************** OVERRIDE *********************************************************
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        initMenu();
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawer.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //endregion OVERRIDE
+
+    //region ******************** INIT *************************************************************
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initMenu() {
+        NavigationView navigationView = findViewById(R.id.nvView);
+        drawer = findViewById(R.id.drawer);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
+        drawer.addDrawerListener(drawerToggle);
+
+        selectDrawerItem(navigationView.getMenu().getItem(0));
+        initDrawerContent(navigationView);
+        navigationView.setItemIconTintList(null);
+    }
+
+    private void initDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    selectDrawerItem(menuItem);
+                    return true;
+                }
+            });
+    }
+
+    //endregion INIT
+
+    //region ******************** HELPERS **********************************************************
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment;
+        switch(menuItem.getItemId()) {
+            case R.id.menuAuthor: fragment = new FragmentAuthor(); break;
+            case R.id.menuFeedback: fragment = new FragmentFeedback(); break;
+            case R.id.menuOrders: fragment = new FragmentMyOrders(); break;
+            default: fragment = new FragmentHookahList(); break;
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawer.closeDrawers();
+    }
+
+    //endregion HELPERS
 
 }
